1f4e934fb0f07a8afc576e70707d39e0039f6449
Khorrshr <finallytheemailwhichisnottaken@gmail.com>
2020-10-07 18:22:28 +0300

    Add files via upload

---
+++ StoryWriteHelper/MainForm.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace StoryWriteHelperLite
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_flavor = new System.Windows.Forms.RichTextBox();
+            this.box_pageNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.box_buttonName1 = new System.Windows.Forms.TextBox();
+            this.box_buttonName2 = new System.Windows.Forms.TextBox();
+            this.box_buttonName3 = new System.Windows.Forms.TextBox();
+            this.box_buttonName4 = new System.Windows.Forms.TextBox();
+            this.box_buttonControl1 = new System.Windows.Forms.TextBox();
+            this.box_buttonControl2 = new System.Windows.Forms.TextBox();
+            this.box_buttonControl3 = new System.Windows.Forms.TextBox();
+            this.box_buttonControl4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.txt_log = new System.Windows.Forms.RichTextBox();
+            this.btn_clearforms = new System.Windows.Forms.Button();
+            this.btn_index = new System.Windows.Forms.Button();
+            this.btn_texts = new System.Windows.Forms.Button();
+            this.btn_names = new System.Windows.Forms.Button();
+            this.btn_controls = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_flavor
+            // 
+            this.txt_flavor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_flavor.Location = new System.Drawing.Point(12, 12);
+            this.txt_flavor.Name = "txt_flavor";
+            this.txt_flavor.Size = new System.Drawing.Size(842, 610);
+            this.txt_flavor.TabIndex = 0;
+            this.txt_flavor.Text = "";
+            // 
+            // box_pageNumber
+            // 
+            this.box_pageNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_pageNumber.Location = new System.Drawing.Point(1028, 44);
+            this.box_pageNumber.Name = "box_pageNumber";
+            this.box_pageNumber.Size = new System.Drawing.Size(100, 29);
+            this.box_pageNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(880, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Page number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(918, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Button text";
+            // 
+            // box_buttonName1
+            // 
+            this.box_buttonName1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonName1.Location = new System.Drawing.Point(877, 144);
+            this.box_buttonName1.Name = "box_buttonName1";
+            this.box_buttonName1.Size = new System.Drawing.Size(199, 29);
+            this.box_buttonName1.TabIndex = 4;
+            // 
+            // box_buttonName2
+            // 
+            this.box_buttonName2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonName2.Location = new System.Drawing.Point(877, 179);
+            this.box_buttonName2.Name = "box_buttonName2";
+            this.box_buttonName2.Size = new System.Drawing.Size(199, 29);
+            this.box_buttonName2.TabIndex = 5;
+            // 
+            // box_buttonName3
+            // 
+            this.box_buttonName3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonName3.Location = new System.Drawing.Point(877, 214);
+            this.box_buttonName3.Name = "box_buttonName3";
+            this.box_buttonName3.Size = new System.Drawing.Size(199, 29);
+            this.box_buttonName3.TabIndex = 6;
+            // 
+            // box_buttonName4
+            // 
+            this.box_buttonName4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonName4.Location = new System.Drawing.Point(877, 249);
+            this.box_buttonName4.Name = "box_buttonName4";
+            this.box_buttonName4.Size = new System.Drawing.Size(199, 29);
+            this.box_buttonName4.TabIndex = 7;
+            // 
+            // box_buttonControl1
+            // 
+            this.box_buttonControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonControl1.Location = new System.Drawing.Point(1082, 144);
+            this.box_buttonControl1.Name = "box_buttonControl1";
+            this.box_buttonControl1.Size = new System.Drawing.Size(76, 29);
+            this.box_buttonControl1.TabIndex = 8;
+            // 
+            // box_buttonControl2
+            // 
+            this.box_buttonControl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonControl2.Location = new System.Drawing.Point(1082, 179);
+            this.box_buttonControl2.Name = "box_buttonControl2";
+            this.box_buttonControl2.Size = new System.Drawing.Size(76, 29);
+            this.box_buttonControl2.TabIndex = 9;
+            // 
+            // box_buttonControl3
+            // 
+            this.box_buttonControl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonControl3.Location = new System.Drawing.Point(1082, 214);
+            this.box_buttonControl3.Name = "box_buttonControl3";
+            this.box_buttonControl3.Size = new System.Drawing.Size(76, 29);
+            this.box_buttonControl3.TabIndex = 10;
+            // 
+            // box_buttonControl4
+            // 
+            this.box_buttonControl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_buttonControl4.Location = new System.Drawing.Point(1082, 249);
+            this.box_buttonControl4.Name = "box_buttonControl4";
+            this.box_buttonControl4.Size = new System.Drawing.Size(76, 29);
+            this.box_buttonControl4.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1078, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ref Num";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.AutoSize = true;
+            this.btn_submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_submit.Location = new System.Drawing.Point(971, 302);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(114, 41);
+            this.btn_submit.TabIndex = 13;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(1146, 1);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(23, 25);
+            this.btn_clear.TabIndex = 14;
+            this.btn_clear.Text = "x";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // txt_log
+            // 
+            this.txt_log.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_log.Location = new System.Drawing.Point(877, 396);
+            this.txt_log.Name = "txt_log";
+            this.txt_log.Size = new System.Drawing.Size(281, 188);
+            this.txt_log.TabIndex = 15;
+            this.txt_log.Text = "";
+            // 
+            // btn_clearforms
+            // 
+            this.btn_clearforms.AutoSize = true;
+            this.btn_clearforms.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clearforms.Location = new System.Drawing.Point(1036, 590);
+            this.btn_clearforms.Name = "btn_clearforms";
+            this.btn_clearforms.Size = new System.Drawing.Size(124, 34);
+            this.btn_clearforms.TabIndex = 16;
+            this.btn_clearforms.Text = "Clear Form";
+            this.btn_clearforms.UseVisualStyleBackColor = true;
+            this.btn_clearforms.Click += new System.EventHandler(this.btn_clearforms_Click);
+            // 
+            // btn_index
+            // 
+            this.btn_index.AutoSize = true;
+            this.btn_index.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_index.Location = new System.Drawing.Point(877, 590);
+            this.btn_index.Name = "btn_index";
+            this.btn_index.Size = new System.Drawing.Size(152, 34);
+            this.btn_index.TabIndex = 17;
+            this.btn_index.Text = "Sort Files";
+            this.btn_index.UseVisualStyleBackColor = true;
+            this.btn_index.Click += new System.EventHandler(this.btn_index_Click);
+            // 
+            // btn_texts
+            // 
+            this.btn_texts.AutoSize = true;
+            this.btn_texts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_texts.Location = new System.Drawing.Point(877, 357);
+            this.btn_texts.Name = "btn_texts";
+            this.btn_texts.Size = new System.Drawing.Size(76, 34);
+            this.btn_texts.TabIndex = 18;
+            this.btn_texts.Text = "Flavor";
+            this.btn_texts.UseVisualStyleBackColor = true;
+            this.btn_texts.Visible = false;
+            // 
+            // btn_names
+            // 
+            this.btn_names.AutoSize = true;
+            this.btn_names.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_names.Location = new System.Drawing.Point(959, 357);
+            this.btn_names.Name = "btn_names";
+            this.btn_names.Size = new System.Drawing.Size(100, 34);
+            this.btn_names.TabIndex = 19;
+            this.btn_names.Text = "Btn names";
+            this.btn_names.UseVisualStyleBackColor = true;
+            this.btn_names.Visible = false;
+            // 
+            // btn_controls
+            // 
+            this.btn_controls.AutoSize = true;
+            this.btn_controls.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_controls.Location = new System.Drawing.Point(1065, 357);
+            this.btn_controls.Name = "btn_controls";
+            this.btn_controls.Size = new System.Drawing.Size(99, 34);
+            this.btn_controls.TabIndex = 20;
+            this.btn_controls.Text = "Btn controls";
+            this.btn_controls.UseVisualStyleBackColor = true;
+            this.btn_controls.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 634);
+            this.Controls.Add(this.btn_controls);
+            this.Controls.Add(this.btn_names);
+            this.Controls.Add(this.btn_texts);
+            this.Controls.Add(this.btn_index);
+            this.Controls.Add(this.btn_clearforms);
+            this.Controls.Add(this.txt_log);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.box_buttonControl4);
+            this.Controls.Add(this.box_buttonControl3);
+            this.Controls.Add(this.box_buttonControl2);
+            this.Controls.Add(this.box_buttonControl1);
+            this.Controls.Add(this.box_buttonName4);
+            this.Controls.Add(this.box_buttonName3);
+            this.Controls.Add(this.box_buttonName2);
+            this.Controls.Add(this.box_buttonName1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.box_pageNumber);
+            this.Controls.Add(this.txt_flavor);
+            this.Name = "MainForm";
+            this.Text = "Story Write Helper Lite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txt_flavor;
+        private System.Windows.Forms.TextBox box_pageNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox box_buttonName1;
+        private System.Windows.Forms.TextBox box_buttonName2;
+        private System.Windows.Forms.TextBox box_buttonName3;
+        private System.Windows.Forms.TextBox box_buttonName4;
+        private System.Windows.Forms.TextBox box_buttonControl1;
+        private System.Windows.Forms.TextBox box_buttonControl2;
+        private System.Windows.Forms.TextBox box_buttonControl3;
+        private System.Windows.Forms.TextBox box_buttonControl4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.RichTextBox txt_log;
+        private System.Windows.Forms.Button btn_clearforms;
+        private System.Windows.Forms.Button btn_index;
+        private System.Windows.Forms.Button btn_texts;
+        private System.Windows.Forms.Button btn_names;
+        private System.Windows.Forms.Button btn_controls;
+    }
+}
+
---
+++ StoryWriteHelper/MainForm.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+//using dictionaries i can probably sort the dumbSort
+
+namespace StoryWriteHelperLite
+{
+    public partial class MainForm : Form
+    {
+        List<TextBox> Names = new List<TextBox>();
+        List<TextBox> Contrls = new List<TextBox>();
+
+        public MainForm()
+        {
+            InitializeComponent();            
+            addFields();
+        }
+
+        
+        
+
+        
+
+        string relativePathToFiles = "GameFiles/";
+        string gameFolder; //see setFilesFolder()
+
+        private void setFilesFolder()
+        {
+            gameFolder = relativePathToFiles;
+        }
+
+        string buttonsFile = "buttons.list";
+        string textsFile = "texts.list";
+        string controlsFile = "buttonControls.list";
+
+
+
+        
+
+        
+
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@textsFile, "");
+            File.WriteAllText(@buttonsFile, "");
+            File.WriteAllText(@controlsFile, "");
+            txt_log.Text += "Files cleared\n";
+        }
+
+        private void clearForms()
+        {            
+            txt_flavor.Text = "";
+            box_pageNumber.Text = "";
+            box_buttonControl1.Text = "";
+            box_buttonControl2.Text = "";
+            box_buttonControl3.Text = "";
+            box_buttonControl4.Text = "";
+            box_buttonName1.Text = "";
+            box_buttonName2.Text = "";
+            box_buttonName3.Text = "";
+            box_buttonName4.Text = "";
+            isOK = true;
+            fuckUp = false;
+            txt_log.Text += "Form cleared\n";
+        }
+
+        private void btn_clearforms_Click(object sender, EventArgs e)
+        {
+            clearForms();
+        }
+
+        private void addFields()
+        {
+            //List<TextBox> Names = new List<TextBox>();
+            Names.Add(box_buttonName1);
+            Names.Add(box_buttonName2);
+            Names.Add(box_buttonName3);
+            Names.Add(box_buttonName4);
+
+            //List<TextBox> Controls = new List<TextBox>();
+            Contrls.Add(box_buttonControl1);
+            Contrls.Add(box_buttonControl2);
+            Contrls.Add(box_buttonControl3);
+            Contrls.Add(box_buttonControl4);        
+        }
+
+        
+        /// ////////////////
+        
+        private string flavorUpdate()
+        {
+            if (flavorIsOK() & !fuckUp)
+            {
+                txt_log.Text += "Content updated for page " + box_pageNumber.Text + "\n";
+                return "/" + box_pageNumber.Text + "*" + txt_flavor.Text + "\n";
+            }
+            else { return null; }
+        }
+
+        
+        private bool flavorIsOK()
+        {
+            try
+            {
+                int pageNum = Int32.Parse(box_pageNumber.Text);
+            }
+            catch
+            {
+                txt_log.Text += "Page Number is not Integer fuckup\n";
+                isOK = false;
+            }
+
+            if (txt_flavor.Text == "")
+            {
+                txt_log.Text += "Flavor Text is empty fuckup\n";
+                isOK = false;
+            }
+            if (box_pageNumber.Text == "")
+            {
+                txt_log.Text += "Page Number is empty fuckup\n";
+                isOK = false;
+            }
+            return isOK;
+        }
+
+
+        
+        private void buttonsUpdate()
+        {
+            string prefix = "/" + box_pageNumber.Text + "*";
+            string buttonNamesStr = "";
+            string buttonControlsStr = "";
+            
+            for (int i = 0; i <= 3; i++)
+            {
+                if (Names[i].Text != "" && Contrls[i].Text != "")
+                {
+                    try
+                    {
+                        int contrlElem = Int32.Parse(Contrls[i].Text);                        
+                    }
+                    catch
+                    {
+                        txt_log.Text += "Provide INTEGER Control fuckup\n";
+                        fuckUp = true;
+                        break;
+                    }
+                    buttonNamesStr += Names[i].Text + ";";
+                    buttonControlsStr += Contrls[i].Text + ";";
+                }
+                /*else
+                {
+                    txt_log.Text += "Provide BOTH Names and Control sequentially fuckup\n";
+                }*/
+
+                //txt_log.Text += Names[i].ToString() + "\n";
+                //txt_log.Text += Contrls[i].ToString() + "\n";
+            }
+            string rdyToWriteNames = prefix + buttonNamesStr;//yourString.Substring(0,(yourString.Length - 1));
+            string rdyToWriteControls = prefix + buttonControlsStr;
+            rdyToWriteNames = rdyToWriteNames.Substring(0, rdyToWriteNames.Length - 1);
+            rdyToWriteControls = rdyToWriteControls.Substring(0, rdyToWriteControls.Length - 1);
+            if (flavorIsOK() & !fuckUp)
+            {
+                File.AppendAllText(@buttonsFile, rdyToWriteNames + "\n");
+                File.AppendAllText(@controlsFile, rdyToWriteControls + "\n");
+            }
+        }
+
+        private void textUpdate()
+        {
+            File.AppendAllText(@textsFile, flavorUpdate());
+        }
+
+
+        private void sortFiles(string fileName) //not finished 
+        {
+            string flavorText;//, buttonText, buttonControls;
+            flavorText = File.ReadAllText(fileName);
+            //buttonText = File.ReadAllText(@buttonsFile);
+            //buttonControls = File.ReadAllText(@controlsFile);
+            List<string> splitterFlavorUnparsed = flavorText.Split('/').ToList();
+            //List<string> splitterButtonsUnparsed = buttonText.Split('/').ToList();
+            //List<string> splitterControlsUnparsed = buttonControls.Split('/').ToList();
+
+            //List<string> splitterFlavor;
+
+            var flavorDict = new Dictionary<int, string>();
+            List<int> indexList = new List<int> { };
+            //List<string> flavors = new List<string> { };
+
+
+            foreach (string elem in splitterFlavorUnparsed)
+            {
+                string[] someArray = elem.Split('*');
+                int index = Int32.Parse(someArray[0]);
+                string content = someArray[1];
+                indexList.Add(index);
+                flavorDict.Add(index, content);
+                //flavors.Add(content);
+            }
+            indexList.Sort();
+            File.WriteAllText(fileName, "");
+            string stringToReturn;
+            int minimal = indexList[0];
+            foreach (int elem in indexList)
+            {
+                string textIndex = elem.ToString();
+                if (elem != minimal) { stringToReturn = "/" + textIndex + "*" + flavorDict[elem]; }
+                else { stringToReturn = textIndex + "*" + flavorDict[elem]; }
+                File.AppendAllText(fileName, stringToReturn);
+            }
+            txt_log.Text += "File " + fileName + " sorted\n";
+
+
+            ////up to there
+
+            /*int previousNumba = 0;
+            txt_log.Text += "\n";
+            bool isSorted = true;
+
+            foreach (int numba in indexList)
+            {
+                if (previousNumba >= numba) { txt_log.Text += "\n(!) Not in place: " + previousNumba + "\n"; isSorted = false; }
+                txt_log.Text += numba + ", ";
+                previousNumba = numba;
+            }
+            if (isSorted) { txt_log.Text += "\nFiles are sorted"; }
+            txt_log.Text += "\n";*/
+        }
+
+
+
+
+
+        bool isOK = true;
+        bool fuckUp = false;
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            txt_log.Text = "";
+            textUpdate();            
+            buttonsUpdate();            
+        }
+
+        private void btn_index_Click(object sender, EventArgs e)
+        {            
+            sortFiles(textsFile);
+            sortFiles(buttonsFile);
+            sortFiles(controlsFile);
+        }
+       
+    }
+}
